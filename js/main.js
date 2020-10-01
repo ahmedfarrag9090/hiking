eef245768694ed9307047fd3932eafffb8b179fb
ahmedfarrag9090 <farrag9090@gmail.com>
2020-10-01 23:01:38 +0200

    Edit

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
-(
-    function app() {
-        const butt = document.querySelector('.side-drawer'),
-            nav = document.querySelector('nav');
-            
-        butt.addEventListener('click', function () {
-            nav.classList.contains('nav-active') ? nav.classList.remove('nav-active') :
-                nav.classList.add('nav-active')
-        });
+window.addEventListener('load', () => {
 
 
-        const landing = document.querySelector('.landing'),
-            letsgo = document.querySelector('.lets-go');
+    const spinner = document.querySelector('.spinner');
+
+    spinner.style.display = 'none';
+    document.body.style.overflowY = 'auto';
 
 
-        window.onscroll = function () {
 
-            if (window.scrollY < letsgo.offsetTop) {
-                landing.style.transform = `translateY(-${window.scrollY}px)`;
-            }
 
+
+    const butt = document.querySelector('.side-drawer'),
+        nav = document.querySelector('nav');
+
+    butt.addEventListener('click', function () {
+        nav.classList.contains('nav-active') ? nav.classList.remove('nav-active') :
+            nav.classList.add('nav-active')
+    });
+
+
+
+
+
+    const landing = document.querySelector('.landing'),
+        letsgo = document.querySelector('.lets-go'),
+        header = document.querySelector('header')
+
+
+    window.onscroll = function () {
+
+        if (this.scrollY < letsgo.offsetTop) {
+            landing.style.transform = `translateY(-${this.scrollY}px)`;
+            header.style.top = '0'
+        }else{
+            header.style.top = '-100%'
         }
+
     }
-)();
+});
